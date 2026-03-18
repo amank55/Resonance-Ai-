a87a16bbb11490b21cb026f4a37cdc599d6f76ba
Aman Kumar <amankumarr2005ak@gmail.com>
2026-03-18 18:35:28 +0530

    fix: update font variables and enhance button functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import { Button } from "@/components/ui/button"
+import { toast } from "sonner"
 const page = () => {
   return (
     <>
     <div>
-      <Button>ClickME</Button>
+      <Button size= "xs" onClick={()=> toast.success("Hello World")}>ClickMEsss</Button>
     </div>
     </>
   )
