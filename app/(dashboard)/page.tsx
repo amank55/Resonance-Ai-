1361ef5870c76097f67096899224cc88b5de95a9
Aman Kumar <amankumarr2005ak@gmail.com>
2026-03-21 16:14:10 +0530

    feat: create initial dashboard page and add env configuration file

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,8 @@
+const page = () => {
+  return (
+    <div className="flex items-center justify-center bg-background">
+      <h1 className="text-2xl font-semibold">Dashboard</h1>
+    </div>
+  )
+}
+export default page
