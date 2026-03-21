987e92d25b7a8cc0c32a683dc4911f17ab75a9b8
Aman Kumar <amankumarr2005ak@gmail.com>
2026-03-21 16:45:52 +0530

    feat: enhance dashboard layout with responsive design and sidebar integration

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
-    <div className="flex items-center justify-center bg-background">
-      <h1 className="text-2xl font-semibold">Dashboard</h1>
+    <div className="flex flex-1 items-center justify-center bg-background">
+      <h1 className="text-3xl font-semibold">Dashboard</h1>
     </div>
   )
 }
