c5f1c1bd80100c88d781cb38f9ee4a37614136fe
Aman Kumar <amankumarr2005ak@gmail.com>
2026-03-22 18:21:22 +0530

    feat: add WavyBackground component for animated background effects and update dependencies

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,9 @@
+import { SidebarTrigger } from "@/components/ui/sidebar"
+
 const page = () => {
   return (
     <div className="flex flex-1 items-center justify-center bg-background">
+     <SidebarTrigger/>
       <h1 className="text-3xl font-semibold">Dashboard</h1>
     </div>
   )
