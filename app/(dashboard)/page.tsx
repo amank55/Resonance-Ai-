c5f9d88d7dbb95bff59a3b37ec0bd5be2af15474
Aman Kumar <amankumarr2005ak@gmail.com>
2026-03-22 18:52:53 +0530

    feat: implement DashboardPage with DashboardView and add PageHeader component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,5 @@
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { DashboardView } from "@/features/dashboard/views/dashboard-view";
 
-const page = () => {
-  return (
-    <div className="flex flex-1 items-center justify-center bg-background">
-     <SidebarTrigger/>
-      <h1 className="text-3xl font-semibold">Dashboard</h1>
-    </div>
-  )
-}
-export default page
+export default function DashboardPage() {
+  return <DashboardView />;
+};
---
+++ features/dashboard/views/dashboard-view.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "@/components/page-header";
+
+
+export function DashboardView() {
+  return (
+    <div className="relative">
+      <PageHeader title="Dashboard" className="lg:hidden" />
+     
+      </div>
+  );
+};
