21e4274ddffc921e00ac2592d23c892fe53b0bb6
Aman Kumar <amankumarr2005ak@gmail.com>
2026-03-23 10:48:36 +0530

    feat: implement HeroPattern component and integrate it into DashboardView refactor: update DashboardHeader component for improved structure and styling

--- features/dashboard/views/dashboard-view.tsx
+++ features/dashboard/views/dashboard-view.tsx
@@ -1,11 +1,13 @@
 import { PageHeader } from "@/components/page-header";
 import { DashboardHeader } from "../components/dashboard-header";
+import { HeroPattern } from "../components/hero-pattern";
 
 
 export function DashboardView() {
   return (
     <div className="relative">
       <PageHeader title="Dashboard" className="lg:hidden" />
+      <HeroPattern/>
      <div className="relative space-y-8 p-4 lg:p-16">
          <DashboardHeader/>
      </div>
