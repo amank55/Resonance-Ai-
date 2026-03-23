1c0a5f4353f738f8b3df493de3029fd31b4f5e42
Aman Kumar <amankumarr2005ak@gmail.com>
2026-03-23 10:34:58 +0530

    feat: update metadata for Resonance app and implement DashboardHeader component

--- features/dashboard/views/dashboard-view.tsx
+++ features/dashboard/views/dashboard-view.tsx
@@ -1,11 +1,14 @@
 import { PageHeader } from "@/components/page-header";
+import { DashboardHeader } from "../components/dashboard-header";
 
 
 export function DashboardView() {
   return (
     <div className="relative">
       <PageHeader title="Dashboard" className="lg:hidden" />
-     
+     <div className="relative space-y-8 p-4 lg:p-16">
+         <DashboardHeader/>
+     </div>
       </div>
   );
 };
